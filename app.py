566ce8159d1e49fe05c3bbec2780120ffdf2c16f
Andrey Anokhin <anokhin.andrey@gmail.com>
2020-04-17 19:27:14 +0300

    simple API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -7,6 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "API for predict service"
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    json_input = request.json
+
+    id = json_input['ID']
+
+    result = {
+        'ID': id,
+        'result': 'prediction'
+        }
+
+    return jsonify(result)
+
 
 
 if __name__ == '__main__':
